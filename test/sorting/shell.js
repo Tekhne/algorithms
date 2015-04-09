ca65108dd8ef4c2c57568684bcb8ca849cac60f5
Peter Havens <Peter.Havens@pinnacol.com>
2015-04-09 08:57:45 -0600

    Clean up test.

--- test/sorting/shell.js
+++ test/sorting/shell.js
@@ -4,15 +4,7 @@ var sort = require('../../lib/sorting/shell');
 describe('sort()', function () {
     it('sorts the given array', function () {
         var actual = [9, 7, 8, 6, 5, 3, 4, 2, 1, 0];
-        var expected = actual.slice(0);
-
-        sort(actual);
-        expected.sort();
-
-        assert.strictEqual(actual.length, expected.length);
-
-        for (var i = 0; i < actual.length; i++) {
-            assert.strictEqual(actual[i], expected[i]);
-        }
+        var expected = actual.slice();
+        assert.deepEqual(sort(actual), expected.sort());
     });
 });
