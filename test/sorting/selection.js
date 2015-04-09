66c28505ba936b049e2fb259189895188ca60a11
Peter Havens <Peter.Havens@pinnacol.com>
2015-04-09 09:40:08 -0600

    Add selection sort.

--- test/sorting/selection.js
+++ test/sorting/selection.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
-var sort = require('../../lib/sorting/shell');
+var sort = require('../../lib/sorting/selection');
 
 describe('sort()', function () {
-    it('sorts the given array using shell sort', function () {
+    it('sorts the given array using selection sort', function () {
         var actual = [9, 7, 8, 6, 5, 3, 4, 2, 1, 0];
         var expected = actual.slice();
         assert.deepEqual(sort(actual), expected.sort());
