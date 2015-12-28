39ee8c920784a81ab5a81093a5474cdeb4478263
Peter Havens <peter.havens@gmail.com>
2015-12-28 14:33:04 -0700

    Add array permutation by randomized swapping.

---
+++ test/permute/in_place.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var permute = require('../../lib/permute/in_place');
+
+describe('permute()', function () {
+  it('permutes the given array by randomized swapping', function () {
+    var array = [1, 2, 3, 4, 5];
+    var permutedArray = permute(array.slice());
+    assert.notDeepEqual(permutedArray, array);
+  });
+});
