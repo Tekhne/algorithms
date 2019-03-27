ca75fc15cdf1718928752c2a4af87a2c8f114eb7
Peter Havens <peter.havens@gmail.com>
2019-03-27 09:19:03 -0600

    Add new insert logic.

--- test/doubly-linked-list.js
+++ test/doubly-linked-list.js
@@ -1,81 +1,86 @@
-'use strict';
+const assert = require('assert').strict;
+const { DoublyLinkedList, Node } = require('../lib/doubly-linked-list');
 
-const assert = require('assert');
-const dll = require('../lib/doubly-linked-list');
-
-describe('doubly linked list', () => {
-  let list;
-  let node1;
-  let node2;
-  let node3;
-
-  beforeEach(() => {
-    node1 = new dll.Node(1, null, null);
-    node2 = new dll.Node(2, null, null);
-    node3 = new dll.Node(3, null, null);
-    list = new dll.DoublyLinkedList(node3);
-    list.insert(node2); // Put at front of list.
-    list.insert(node1); // Put at front of list.
-  });
-
-  describe('search()', () => {
-    it('returns null if given key does not match any node.key', () => {
-      assert.equal(list.search(9), null);
+describe('Node', function() {
+  describe('constructor()', function() {
+    it('sets data, next, and previous instance variables', function() {
+      const data = 'test data';
+      const next = new Node();
+      const previous = new Node();
+      const node = new Node({ data, next, previous });
+      assert.equal(node.data, data);
+      assert.equal(node.next, next);
+      assert.equal(node.previous, previous);
     });
+  });
+});
 
-    it('returns node if given key matches a node.key', () => {
-      assert.equal(list.search(1), node1);
+describe('DoublyLinkedList', function() {
+  describe('constructor()', function() {
+    it('sets head instance variable', function() {
+      const head = new Node();
+      const list = new DoublyLinkedList(head);
+      assert.equal(list.head, head);
     });
   });
 
-  describe('insert()', () => {
-    it('sets given node.next to old head of list', () => {
-      let node4 = new dll.Node(4, null, null);
-      list.insert(node4);
-      assert.equal(node4.next, node1);
+  describe('insert()', function() {
+    it('can insert a node before the head', function() {
+      const node1 = new Node({ data: 'node1' });
+      const node2 = new Node({ data: 'node2' });
+      const list = new DoublyLinkedList(node1);
+      list.insert(node2, 1);
+      assert.equal(list.head, node2);
+      assert.equal(list.head.previous, null);
+      assert.equal(list.head.next, node1);
     });
 
-    describe('when old head is not null', () => {
-      it('sets node.previous of old head to given node', () => {
-        let node4 = new dll.Node(4, null, null);
-        list.insert(node4);
-        assert.equal(node1.previous, node4);
-      });
-
-      it('sets head to given node', () => {
-        let node4 = new dll.Node(4, null, null);
-        list.insert(node4);
-        assert.equal(list.head, node4);
-      });
-
-      it('sets node.previous of given node to null', () => {
-        let node4 = new dll.Node(4, null, null);
-        list.insert(node4);
-        assert.equal(node4.previous, null);
-      });
-    });
-  });
-
-  describe('delete()', () => {
-    describe('when given node.previous is not null', () => {
-      it('sets given node.previous.next to given node.next', () => {
-        list.delete(node2);
-        assert.equal(node1.next, node3);
+    describe('when DoublyLinkedList is empty', function() {
+      it('inserts node at the head', function() {
+        const list = new DoublyLinkedList();
+        const node1 = new Node({ data: 'node1' });
+        list.insert(node1, 1);
+        assert.equal(list.head, node1);
+        assert.equal(list.head.next, null);
+        assert.equal(list.head.previous, null);
       });
     });
 
-    describe('when given node.previous is null', () => {
-      it('sets list head to given node.next', () => {
-        list.delete(node1);
-        assert.equal(list.head, node2);
-      });
+    it('can insert a node in the middle', function() {
+      const node1 = new Node({ data: 'node1' });
+      const node2 = new Node({ data: 'node2' });
+      const node3 = new Node({ data: 'node3' });
+      node1.next = node3;
+      const list = new DoublyLinkedList(node1);
+      list.insert(node2, 2);
+
+      assert.equal(list.head, node1);
+      assert.equal(list.head.next, node2);
+      assert.equal(list.head.next.next, node3);
+      assert.equal(list.head.next.next.next, null);
+
+      assert.equal(list.head.previous, null);
+      assert.equal(list.head.next.previous, node1);
+      assert.equal(list.head.next.next.previous, node2);
     });
 
-    describe('when given node.next is not null', () => {
-      it('sets given node.next.previous to given node.previous', () => {
-        list.delete(node2);
-        assert.equal(node2.next.previous, node2.previous);
-      });
+    it('can insert a node after the tail', function() {
+      const node1 = new Node({ data: 'node1' });
+      const node2 = new Node({ data: 'node2' });
+      const node3 = new Node({ data: 'node3' });
+      node1.next = node2;
+      node2.previous = node1;
+      const list = new DoublyLinkedList(node1);
+      list.insert(node3, 3);
+
+      assert.equal(list.head, node1);
+      assert.equal(list.head.next, node2);
+      assert.equal(list.head.next.next, node3);
+      assert.equal(list.head.next.next.next, null);
+
+      assert.equal(list.head.previous, null);
+      assert.equal(list.head.next.previous, node1);
+      assert.equal(list.head.next.next.previous, node2);
     });
   });
 });
